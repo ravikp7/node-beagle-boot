0d128c1ab73a2b66ad2b797f120eee50ad03547f
ravikp7 <7ravikp@gmail.com>
2017-06-02 21:29:23 +0530

    add size of all packets

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ const SPLPID = 0xA4A2;
 const ETHIPP = 0x0800;
 const ETHARPP = 0x0806;
 
+// Size of all packets
+var rndisSize = 44;
+var etherSize = 14;
+var arp_Size = 28;
+var ipSize = 20;
+var udpSize = 8;
+var bootpSize = 300;
+var tftpSize = 4;
+var fullSize = 386;
+
 
 // Include modules
 var usb = require('usb');
