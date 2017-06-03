2d91693653ec388656bf732cec69835b81ec6bf8
ravikp7 <7ravikp@gmail.com>
2017-06-04 00:59:06 +0530

    fix rndis buffer and toggle endianness for rndis

--- src/protocols.js
+++ src/protocols.js
@@ -1,6 +1,7 @@
 var sp = require('schemapack');             // Serialization module
 var bp = require('binary-parser');          // Binary parser module
 var Parser = bp.Parser;
+var toggle = require('endian-toggle');      
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -8,19 +9,23 @@ var Parser = bp.Parser;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // rndis header for encoding
-var rndis_e = sp.build({
-    msg_type: 'uint32',      // always
-    msg_len: 'uint32',       // length of header + data + payload
-    data_offset: 'uint32',   // offset from data until payload
-    data_len: 'uint32',      // length of payload
-    band_offset: 'uint32',   // not used here
-    band_len: 'uint32',      // not used here
-    out_band_elements: 'uint32', //not used here
-    packet_offset: 'uint32',     // not used here
-    packet_info_len: 'uint32',   // not used here
-    reserved_first: 'uint32',    // not used here
-    reserved_second: 'uint32'   // not used here
-    });
+var rndis_1 = sp.build([
+    { msg_type: 'uint32'},      // always
+    { msg_len: 'uint32'},       // length of header + data + payload
+    { data_offset: 'uint32'},   // offset from data until payload
+    { data_len: 'uint32'},      // length of payload
+    { band_offset: 'uint32'},   // not used here
+    { pad: 'string'}
+]);
+var rndis_2 = sp.build([
+    { band_len: 'uint32'},      // not used here
+    { out_band_elements: 'uint32'}, //not used here
+    { packet_offset: 'uint32'},     // not used here
+    { packet_info_len: 'uint32'},   // not used here
+    { reserved_first: 'uint32'},    // not used here
+    { reserved_second: 'uint32'},   // not used here
+    { pad: 'string'}
+]);
 
 // ether2 header for encoding
 var ethhdr_e = sp.build([
@@ -78,22 +83,26 @@ var ethhdr = new Parser()
 
 // Function for rndis data packet
 function make_rndis(data_length){
-    var rndis = {
-        msg_type: 0x00000001,
-        msg_len: data_length+44,
-        data_offset: 0x24,
-        data_len: data_length,
-        band_offset: 0,
-        band_len: 0,
-        out_band_elements: 0,
-        packet_offset: 0,
-        packet_info_len: 0,
-        reserved_first: 0,
-        reserved_second: 0
-    };
+    var rndis1 = [
+        { msg_type: 0x00000001},
+        { msg_len: data_length+44},
+        { data_offset: 0x24},
+        { data_len: data_length},
+        { band_offset: 0}
+    ];
+    var rndis2 =[
+        { band_len: 0},
+        { out_band_elements: 0},
+        { packet_offset: 0},
+        { packet_info_len: 0},
+        { reserved_first: 0},
+        { reserved_second: 0}
+    ];
 
-    var buf = rndis_e.encode(rndis);
-    return buf;
+    var buf1 = fix_buff(rndis_1.encode(rndis1));
+    var buf2 = fix_buff(rndis_2.encode(rndis2));
+    var data = Buffer.concat([buf1, buf2], 44);
+    return toggle(data, 32);
 }
 
 
