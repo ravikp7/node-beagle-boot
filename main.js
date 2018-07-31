9eaa6717d33542376abdc8a1f3727e5172ef6727
ravikp7 <7ravikp@gmail.com>
2018-08-01 01:23:56 +0530

    refactor proxy server code

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const platform = os.platform();
 const rndis_init = require('./lib/rndis_init');
 const emitterMod = new EventEmitter(); // Emitter for module status
 const capture = new cap();
-const proxyUtils = require('./lib/proxy-utils');
+const proxy = require('./lib/proxy');
 const identifyRequest = require('./lib/identifyRequest');
 const constants = require('./lib/constants');
 
@@ -122,17 +122,17 @@ emitter.on('configureProxy', () => {
   network.get_active_interface((error, activeInterface) => {
     if (!error) {
       proxyConfig.Host = {
-        SourceMac: proxyUtils.getAddressArray(activeInterface.mac_address),
-        SourceIp: proxyUtils.getAddressArray(activeInterface.ip_address),
-        GatewayIp: proxyUtils.getAddressArray(activeInterface.gateway_ip)
+        SourceMac: proxy.getAddressArray(activeInterface.mac_address),
+        SourceIp: proxy.getAddressArray(activeInterface.ip_address),
+        GatewayIp: proxy.getAddressArray(activeInterface.gateway_ip)
       };
       proxyConfig.BB = {
         SourceIp: [192, 168, 6, 2],
         GatewayIp: [192, 168, 6, 1],
       };
       proxyConfig.ActiveInterface = activeInterface;
-      proxyUtils.getAvailableIp(activeInterface.ip_address).then((proxyIp) => {
-        proxyConfig.ProxyIp = proxyUtils.getAddressArray(proxyIp);
+      proxy.getAvailableIp(activeInterface.ip_address).then((proxyIp) => {
+        proxyConfig.ProxyIp = proxy.getAddressArray(proxyIp);
         console.log(`Using Proxy IP Address: ${proxyIp}`);
       });
     }
@@ -243,80 +243,7 @@ const onOpen = (server) => {
         capture.open(device, filter, bufSize, buffer);
 
         capture.on('packet', () => {
-          const request = identifyRequest(server, buffer);
-          if (request === 'ARP') {
-            const receivedARP = protocols.parse_arp(buffer.slice(constants.ETHER_SIZE));
-            if (receivedARP.ip_dest[3] === proxyConfig.ProxyIp[3]) {
-
-              // Response Host.GatewayIp is at Host.GatewayMac
-              if (receivedARP.opcode === constants.ARP_OPCODE_REPLY) {
-                if (receivedARP.ip_source[3] === proxyConfig.Host.GatewayIp[3]) {
-                  proxyConfig.Host.GatewayMac = receivedARP.hw_source;
-                  const etherSrc = proxyConfig.BB.GatewayMac;
-                  const etherDst = proxyConfig.BB.SourceMac;
-                  const ipSrc = proxyConfig.BB.GatewayIp;
-                  const ipDst = proxyConfig.BB.SourceIp;
-                  const arpHeader = protocols.make_arp(constants.ARP_OPCODE_REPLY, etherSrc, ipSrc, etherDst, ipDst);
-                  const etherHeader = protocols.make_ether2(etherDst, etherSrc, constants.ETH_TYPE_ARP);
-                  const proxyArp = Buffer.concat([etherHeader, arpHeader], constants.ETHER_SIZE + constants.ARP_SIZE);
-                  emitter.emit('outTransfer', server, proxyArp, 'ARP');
-                }
-                else {
-                  proxyConfig.ArpList[receivedARP.ip_source[3]] = receivedARP.hw_source;
-                }
-              }
-
-              // Request Who has ProxyIp ? Tell SomeIp
-              if (receivedARP.opcode === constants.ARP_OPCODE_REQUEST) {
-                const etherSrc = proxyConfig.Host.SourceMac;
-                const etherDst = receivedARP.hw_source;
-                const ipSrc = proxyConfig.ProxyIp;
-                const ipDst = receivedARP.ip_source;
-                const arpHeader = protocols.make_arp(constants.ARP_OPCODE_REPLY, etherSrc, ipSrc, etherDst, ipDst);
-                const etherHeader = protocols.make_ether2(etherDst, etherSrc, constants.ETH_TYPE_ARP);
-                const proxyArp = Buffer.concat([etherHeader, arpHeader], constants.ETHER_SIZE + constants.ARP_SIZE);
-                sendToNetwork(proxyArp);
-              }
-            }
-          }
-
-          else {
-            const etherSrc = proxyConfig.BB.GatewayMac;
-            const etherDst = proxyConfig.BB.SourceMac;
-            const receivedIP = protocols.parseIpv4(buffer.slice(constants.ETHER_SIZE));
-            if (receivedIP.DestinationAddress[3] === proxyConfig.ProxyIp[3]) {
-              let ipSrc;
-              if (proxyUtils.compareIp(receivedIP.SourceAddress, proxyConfig.Host.GatewayIp)) {
-                ipSrc = proxyConfig.BB.GatewayIp;
-              }
-              else ipSrc = receivedIP.SourceAddress;
-              const ipHeader = {
-                Version: receivedIP.Version,
-                IHL: receivedIP.IHL,
-                TypeOfService: receivedIP.TypeOfService,
-                TotalLength: receivedIP.TotalLength,
-                Identification: receivedIP.Identification,
-                Flags: receivedIP.Flags,
-                FragmentOffset: receivedIP.FragmentOffset,
-                TimeToLIve: receivedIP.TimeToLIve,
-                Protocol: receivedIP.Protocol,
-                HeaderChecksum: 0,
-                SourceAddress: ipSrc,
-                DestinationAddress: proxyConfig.BB.SourceIp
-              };
-              let ipPayload = buffer.slice(constants.ETHER_SIZE + constants.IPV4_SIZE, constants.ETHER_SIZE + ipHeader.TotalLength);
-              if (ipHeader.Protocol === constants.IP_TCP) {
-                ipPayload = protocols.regenerateTcpChecksum(ipHeader, ipPayload);
-              }
-              if (ipHeader.Protocol === constants.IP_UDP) {
-                ipPayload = Buffer.concat([ipPayload.slice(0, 6), Buffer.from([0, 0]), ipPayload.slice(8)]);
-              }
-              const ipBuff = protocols.encodeIpv4(ipHeader);
-              const etherBuff = protocols.make_ether2(etherDst, etherSrc, constants.ETH_TYPE_IPV4);
-              const changedPacket = Buffer.concat([etherBuff, ipBuff, ipPayload]);
-              emitter.emit('outTransfer', server, changedPacket, 'IP');
-            }
-          }
+          proxy.processIn(server, capture, buffer, proxyConfig, emitter);
         });
         emitter.emit('inTransfer', server);
 
@@ -343,114 +270,12 @@ const onOpen = (server) => {
   }
 };
 
-const sendToNetwork = (data) => {
-  try {
-
-    // send will not work if pcap_sendpacket is not supported by underlying `device`
-    capture.send(data, data.length);
-  } catch (e) {
-    console.log(`Error sending packet: ${e}`);
-  }
-};
-
-
 // Event for inEnd transfer
 emitter.on('inTransfer', (server) => {
   server.inEndpoint.on('data', (data) => {
 
     if (server.foundDevice === constants.LINUX_COMPOSITE_DEVICE) {
-      const etherHeader = protocols.decode_ether(data);
-      // Update Source and Gateway Mac for BeagleBone
-      if (!proxyConfig.BB.SourceMac) {
-        proxyConfig.BB.SourceMac = etherHeader.h_source;
-        let tempMac = etherHeader.h_source;
-        let gatewayMac = [];
-        tempMac.forEach((part) => {
-          if (tempMac.indexOf(part) === 5) gatewayMac.push(part - 1);
-          else gatewayMac.push(part);
-        });
-        proxyConfig.BB.GatewayMac = gatewayMac;
-      }
-      const request = identifyRequest(server, data);
-      if (request === 'ARP') {
-        const receivedARP = protocols.parse_arp(data.slice(constants.ETHER_SIZE));
-
-        // ARP request Who has 192.168.6.1? Tell 192.168.6.2
-        if (receivedARP.opcode === constants.ARP_OPCODE_REQUEST && receivedARP.ip_dest[2] === 6) {
-
-          // Change it to Who has Host.GatewayIP? Tell ProxyIp
-          const etherSrc = proxyConfig.Host.SourceMac;
-          const etherDst = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
-          const ipSrc = proxyConfig.ProxyIp;
-          const ipDst = proxyConfig.Host.GatewayIp;
-          const arpHeader = protocols.make_arp(constants.ARP_OPCODE_REQUEST, etherSrc, ipSrc, etherDst, ipDst);
-          const etherHeader = protocols.make_ether2(etherDst, etherSrc, constants.ETH_TYPE_ARP);
-          const changedPacket = Buffer.concat([etherHeader, arpHeader]);
-          sendToNetwork(changedPacket);
-        }
-      }
-      else {
-        const etherSrc = proxyConfig.Host.SourceMac;
-        let etherDst;
-        const ether = protocols.decode_ether(data);
-        if (proxyConfig.Host.GatewayMac) {
-          etherDst = proxyConfig.Host.GatewayMac;
-        }
-        else etherDst = ether.h_dest;
-        const receivedIP = protocols.parseIpv4(data.slice(constants.ETHER_SIZE));
-        let ipDst;
-        if (receivedIP.DestinationAddress[0] === proxyConfig.BB.GatewayIp[0]) {
-          if (receivedIP.DestinationAddress[2] === proxyConfig.BB.GatewayIp[2]) {
-            ipDst = proxyConfig.Host.GatewayIp;
-          }
-          else {
-            ipDst = receivedIP.DestinationAddress;
-            if (proxyConfig.ArpList[receivedIP.DestinationAddress[3]]) {
-              etherDst = proxyConfig.ArpList[receivedIP.DestinationAddress[3]];
-            }
-            else {
-              // Send ARP for host in local subnet
-              const etherSrc = proxyConfig.Host.SourceMac;
-              const etherDst = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
-              const ipSrc = proxyConfig.ProxyIp;
-              const ipDst = receivedIP.DestinationAddress;
-              const arpHeader = protocols.make_arp(constants.ARP_OPCODE_REQUEST, etherSrc, ipSrc, etherDst, ipDst);
-              const etherHeader = protocols.make_ether2(etherDst, etherSrc, constants.ETH_TYPE_ARP);
-              const changedPacket = Buffer.concat([etherHeader, arpHeader]);
-              sendToNetwork(changedPacket);
-            }
-            if (proxyUtils.compareIp(receivedIP.DestinationAddress, proxyConfig.Host.SourceIp)) {
-              etherDst = proxyConfig.Host.SourceMac;
-            }
-          }
-        }
-        else ipDst = receivedIP.DestinationAddress;
-        const ipHeader = {
-          Version: receivedIP.Version,
-          IHL: receivedIP.IHL,
-          TypeOfService: receivedIP.TypeOfService,
-          TotalLength: receivedIP.TotalLength,
-          Identification: receivedIP.Identification,
-          Flags: receivedIP.Flags,
-          FragmentOffset: receivedIP.FragmentOffset,
-          TimeToLIve: receivedIP.TimeToLIve,
-          Protocol: receivedIP.Protocol,
-          HeaderChecksum: 0,
-          SourceAddress: proxyConfig.ProxyIp,
-          DestinationAddress: ipDst
-        };
-        let ipPayload = data.slice(constants.ETHER_SIZE + constants.IPV4_SIZE, constants.ETHER_SIZE + ipHeader.TotalLength);
-        if (ipHeader.Protocol === constants.IP_TCP) {
-          ipPayload = protocols.regenerateTcpChecksum(ipHeader, ipPayload);
-        }
-        if (ipHeader.Protocol === constants.IP_UDP) {
-          ipPayload = Buffer.concat([ipPayload.slice(0, 6), Buffer.from([0, 0]), ipPayload.slice(8)]);
-        }
-        const ipBuff = protocols.encodeIpv4(ipHeader);
-        const etherBuff = protocols.make_ether2(etherDst, etherSrc, constants.ETH_TYPE_IPV4);
-        const changedPacket = Buffer.concat([etherBuff, ipBuff, ipPayload]);
-        sendToNetwork(changedPacket);
-      }
+      proxy.processOut(server, capture, data, proxyConfig);
     }
     else {
       const request = identifyRequest(server, data);
