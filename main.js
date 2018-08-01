d59558c5d587fc255071056124f5b36dd7601e83
ravikp7 <7ravikp@gmail.com>
2018-08-01 10:20:57 +0530

    refactor rndis init

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const network = require('network');
 const cap = require('cap').Cap;
 const os = require('os');
 const platform = os.platform();
-const rndis_init = require('./lib/rndis_init');
+const rndisInit = require('./lib/rndis_init');
 const emitterMod = new EventEmitter(); // Emitter for module status
 const capture = new cap();
 const proxy = require('./lib/proxy');
@@ -172,48 +172,9 @@ const onOpen = (server) => {
   }
   updateProgress('Interface claimed');
 
-  // Code to initialize RNDIS device on Windows and OSX
+  // Initialize RNDIS device on Windows and OSX
   if (platform != 'linux' && server.foundDevice == constants.ROM) {
-    const intf0 = server.device.interface(0); // Select interface 0 for CONTROL transfer
-    intf0.claim();
-    const CONTROL_BUFFER_SIZE = 1025;
-    const RNDIS_INIT_SIZE = 24;
-    const RNDIS_SET_SIZE = 28;
-    const rndis_buf = Buffer.alloc(CONTROL_BUFFER_SIZE);
-    const init_msg = rndis_init.make_rndis_init();
-    init_msg.copy(rndis_buf, 0, 0, RNDIS_INIT_SIZE);
-
-    // Windows Control Transfer
-    // https://msdn.microsoft.com/en-us/library/aa447434.aspx
-    // http://www.beyondlogic.org/usbnutshell/usb6.shtml
-    const bmRequestType_send = 0x21; // USB_TYPE=CLASS | USB_RECIPIENT=INTERFACE
-    const bmRequestType_receive = 0xA1; // USB_DATA=DeviceToHost | USB_TYPE=CLASS | USB_RECIPIENT=INTERFACE
-
-    // Sending rndis_init_msg (SEND_ENCAPSULATED_COMMAND)
-    server.device.controlTransfer(bmRequestType_send, 0, 0, 0, rndis_buf, () => {
-      // This error doesn't affect the functionality, so ignoring
-      //if(error) emitterMod.emit('error', "Control transfer error on SEND_ENCAPSULATED " +error);
-    });
-
-    // Receive rndis_init_cmplt (GET_ENCAPSULATED_RESPONSE)
-    server.device.controlTransfer(bmRequestType_receive, 0x01, 0, 0, CONTROL_BUFFER_SIZE, (error) => {
-      if (error) emitterMod.emit('error', `Control transfer error on GET_ENCAPSULATED ${error}`);
-    });
-
-
-    const set_msg = rndis_init.make_rndis_set();
-    set_msg.copy(rndis_buf, 0, 0, RNDIS_SET_SIZE + 4);
-
-    // Send rndis_set_msg (SEND_ENCAPSULATED_COMMAND)
-    server.device.controlTransfer(bmRequestType_send, 0, 0, 0, rndis_buf, () => {
-      // This error doesn't affect the functionality, so ignoring
-      //if(error) emitterMod.emit('error', "Control transfer error on SEND_ENCAPSULATED " +error);
-    });
-
-    // Receive rndis_init_cmplt (GET_ENCAPSULATED_RESPONSE)
-    server.device.controlTransfer(bmRequestType_receive, 0x01, 0, 0, CONTROL_BUFFER_SIZE, (error) => {
-      if (error) emitterMod.emit('error', `Control transfer error on GET_ENCAPSULATED ${error}`);
-    });
+    rndisInit(server);
   }
 
   if (server.foundDevice === constants.LINUX_COMPOSITE_DEVICE) {
